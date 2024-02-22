620b0ef6ed65893ba974c319bf563cb9759fd232
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-22 12:50:41 +0300

    Add logo image and constants folder

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
+import Sidebar from '@/components/share/Sidebar'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
         <main className='root'>
+            <Sidebar />
+            {/* <MobileNav/> */}
+
             <div className="root-container">
                 <div className="wrapper">
                     {children}
